be5cdc05678422247015855528a3b5318b6dfe64
Jeffrey Walls <fr0st3e5@mail.com>
2023-11-15 14:08:46 -0700

    Output redirect and cd

--- myShell/myShell.py
+++ myShell/myShell.py
@@ -6,41 +6,42 @@ pid = os.getpid()
 
 while 1:
     os.write(1,("$").encode())
-    cmd = os.read(0, 50).decode().strip()
-    if(cmd == "exit"):
+    cmd = os.read(0, 100).decode().strip()
+    args = cmd.split(" ")
+    if(len(cmd)==0):
+        pass
+    elif(args[0] == "exit"):
         break
+    elif(args[0]=="cd"):
+        os.chdir(args[1])
+    else:
+        
+        rc = os.fork()
 
-    rc = os.fork()
+        if rc < 0:
+            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            sys.exit(1)
 
-    if rc < 0:
-        os.write(2, ("fork failed, returning %d\n" % rc).encode())
-        sys.exit(1)
+        elif rc == 0: #Child
+            os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                    (os.getpid(), pid)).encode())
+            for i in range(len(args)-1):
+                if(args[i]==">"): #output redirection
+                    os.close(1)
+                    fa = str(os.open(args[i+1], os.O_WRONLY | os.O_CREAT))
+                    os.set_inheritable(1, True)
+                    n = len(args)
+                    for j in range(0, n-i):
+                        args.pop()
+            for dir in re.split(":", os.environ['PATH']): # try each directory in the path
+                program = "%s/%s" % (dir, args[0])
+                try:
+                    os.execve(program, args, os.environ) # try to exec program
+                except FileNotFoundError:             # ...expected
+                    pass                              # ...fail quietly
+            os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)                 # terminate with error
 
-    elif rc == 0: #Child
-        os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                 (os.getpid(), pid)).encode())
-        args = cmd.split(" ")
-        for i in range(len(args)-1):
-            if(args[i]==">"):
-                os.close(1)
-                fa = str(os.open(args[i+1], os.O_WRONLY | os.O_CREAT))
-                os.set_inheritable(1, True)
-                n = len(args)
-                for j in range(0, n-i):
-                    args.pop()
-        for dir in re.split(":", os.environ['PATH']): # try each directory in the path
-            program = "%s/%s" % (dir, args[0])
-            os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
-            try:
-                os.execve(program, args, os.environ) # try to exec program
-            except FileNotFoundError:             # ...expected
-                pass                              # ...fail quietly
-        os.write(2, ("Child:    Could not exec %s\n" % args[0]).encode())
-        sys.exit(1)                 # terminate with error
-
-    else:                           # Parent (forked ok)
-        os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" % 
-                    (pid, rc)).encode())
-        childPidCode = os.wait()
-        os.write(1, ("Parent: Child %d terminated with exit code %d\n" % 
-                    childPidCode).encode())
+        else:                           # Parent (forked ok)
+            if(not cmd[len(cmd)-1]=="$"):
+                childPidCode = os.wait()
